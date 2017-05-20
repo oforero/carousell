9ef2310a77888fdb9d833cdfeea2a7bdc2b4e28f
Oscar Forero <oscar.forero@gatech.edu>
2017-05-20 12:33:16 +0800

    Move to API subproject

--- api/src/main.go
+++ api/src/main.go
@@ -7,7 +7,7 @@ import (
 type helloHandler struct{}
 
 func (h helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Go!"))
+	w.Write([]byte("Hello again Go!"))
 }
 
 func main() {
